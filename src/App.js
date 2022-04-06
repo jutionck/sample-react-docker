6dd9ccfef2a7f4e345182acbb48b936cf14b00cd
Jution Candra Kirana <jutionck@gmail.com>
2022-04-06 09:59:47 +0700

    List Todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import {getTodos} from '../src/api/todosService'
+import Todo from './Todo';
+import { Button, Card, Form } from 'react-bootstrap';
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
+  const loadData = () => {
+    getTodos().then((response) => {
+      console.log(response.data.data);
+      setTodos(response.data.data)
+    })
+    .catch((e) => {
+      console.log(e);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
